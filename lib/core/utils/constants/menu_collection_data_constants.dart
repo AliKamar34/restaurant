11016490199e0966b7c6ffb0561977497feae725
Ali kamar <aliekamar123@gmail.com>
2025-07-24 19:20:37 +0300

    create  menu collection

---
+++ lib/core/utils/constants/menu_collection_data_constants.dart
@@ -0,0 +1,6 @@
+class MenuCollectionDataConstants {
+  static const String menuCollection = 'menu';
+  static const String itemImage = 'image';
+  static const String itemName = 'name';
+  static const String itemPrice = 'price';
+}
