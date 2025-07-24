9b3f14f01db32689b7a62c1a3bd4a9f409335340
Ali kamar <aliekamar123@gmail.com>
2025-07-24 19:25:25 +0300

    add dependeces

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:restaurant/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:restaurant/features/auth/presentation/manager/auth_bloc/auth_bloc.dart';
 import 'package:restaurant/features/auth/presentation/views/log_in_view.dart';
 import 'package:restaurant/features/auth/presentation/views/sign_up_view.dart';
+import 'package:restaurant/features/home/presentation/views/nav_bar.dart';
 
 abstract class AppRoutes {
   static const String kLogInView = '/logInView';
   static const String kSignUpView = '/signUpView';
+  static const String kNavBarView = '/navBar';
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
-          return const LogInView();
+          return BlocProvider(
+            create: (context) => AuthBloc(AuthRepoImpl()),
+            child: const LogInView(),
+          );
         },
       ),
       GoRoute(
         path: kSignUpView,
         builder: (context, state) {
-          return const SignUpView();
+          return BlocProvider(
+            create: (context) => AuthBloc(AuthRepoImpl()),
+            child: const SignUpView(),
+          );
+        },
+      ),
+      GoRoute(
+        path: kNavBarView,
+        builder: (context, state) {
+          return NavBarView();
         },
       ),
     ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurant/core/helpers/bloc_opserver.dart';
 import 'package:restaurant/restaurant_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -6,5 +8,6 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = SimpleBlocObserver();
   runApp(const RestaurantApp());
 }
