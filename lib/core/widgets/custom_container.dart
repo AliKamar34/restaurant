dfc4386e8a1553263fbea8a9bc9c12a47ff8693a
Ali kamar <aliekamar123@gmail.com>
2025-07-24 19:21:18 +0300

    create custom cintainer

---
+++ lib/core/widgets/custom_container.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    super.key,
+    required this.child,
+    this.width,
+    this.hight,
+  });
+  final Widget child;
+  final double? width;
+  final double? hight;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      height: hight,
+      width: width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: child,
+    );
+  }
+}
