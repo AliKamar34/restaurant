27cfdc918f269974704c771439404fa5249b3eca
Ali kamar <aliekamar123@gmail.com>
2025-07-25 23:01:36 +0300

    create cart collection data constants

---
+++ lib/core/utils/constants/cart_collection_data_constant.dart
@@ -0,0 +1,7 @@
+class CartCollectionDataConstant {
+  static const String cartCollectionName = 'cart';
+  static const String cartItemName = 'name';
+  static const String cartItemImage = 'iamge';
+  static const String cartItemPrice = 'price';
+  static const String cartItemQuantity = 'quantity';
+}
